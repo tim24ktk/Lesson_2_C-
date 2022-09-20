ee4b296d89b66b2df99203dcc0bfcc9abd7528bd
tim24ktk <tim24ktk@mail.ru>
2022-09-20 23:55:22 +0300

    homework_2

---
+++ task_10/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+    Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+    456 -> 5
+    782 -> 8
+    918 -> 1
+*/
+
+int GetNumber()
+{
+    Console.WriteLine("Введите трёхзначное число");
+    string numberStr = Console.ReadLine();
+    int number = int.Parse(numberStr);
+
+    return number;
+}
+
+int number = GetNumber();
+
+if (number.ToString().Length == 3)
+{
+    Console.WriteLine((number / 10) % 10);
+}
+else
+{
+    Console.WriteLine("Вы ввели число отличное от трёхзначного!");
+}
+
---
+++ task_13/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+    Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+    645 -> 5
+
+    78 -> третьей цифры нет
+
+    32679 -> 6
+*/
+
+string GetNumber()
+{
+    Console.WriteLine("Введите число");
+    string numberStr = Console.ReadLine();
+
+    return numberStr;
+}
+
+string number = GetNumber();
+
+if (number.Length >= 3)
+{
+    Console.WriteLine(number[2]);
+}
+else
+{
+    Console.WriteLine("В введенном вами числе третьей цифры нет!");
+}
